ea09ead381c2e552a68aa6fb9e9598fba296be7a
alirezaghnzd <changlee76301@gmail.com>
2025-08-02 15:41:21 +0330

    add styled components and work with on for practice for project and learn more

--- apps/hotel-management/src/App.tsx
+++ apps/hotel-management/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const StyleApp = styled.div`
+  background-color: cyan;
+  padding: 2rem 1.6rem;
+`;
+const Button = styled.button`
+  border: none;
+  border-radius: 1.2rem;
+  background-color: #da6d6d;
+  color: white;
+  padding: 1.2rem 3rem;
+`;
+const H1 = styled.h1`
+  font-weight: bold;
+  font-size: 20px;
+`;
+const Input = styled.input`
+  border: none;
+  padding: 1rem 5rem;
+  border-radius: 0.5rem;
+`;
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyleApp>
+      <Input></Input>
+      <H1>Hello world</H1>
+      <Button onClick={() => alert("1")}>Add</Button>
+      <Button onClick={() => alert("1")}>Add</Button>
+    </StyleApp>
+  );
 }
-
-export default App
