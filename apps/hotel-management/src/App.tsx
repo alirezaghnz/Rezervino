94393dd4eee3ea401a42244477bd78cffe93047f
alirezaghnzd <changlee76301@gmail.com>
2025-08-03 13:49:21 +0330

    add styles folder and ui for reusable component style, like button, Input

--- apps/hotel-management/src/App.tsx
+++ apps/hotel-management/src/App.tsx
@@ -1,33 +1,28 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const StyleApp = styled.div`
   background-color: cyan;
   padding: 2rem 1.6rem;
 `;
-const Button = styled.button`
-  border: none;
-  border-radius: 1.2rem;
-  background-color: #da6d6d;
-  color: white;
-  padding: 1.2rem 3rem;
-`;
+
 const H1 = styled.h1`
   font-weight: bold;
   font-size: 20px;
 `;
-const Input = styled.input`
-  border: none;
-  padding: 1rem 5rem;
-  border-radius: 0.5rem;
-`;
 
 export default function App() {
   return (
-    <StyleApp>
-      <Input></Input>
-      <H1>Hello world</H1>
-      <Button onClick={() => alert("1")}>Add</Button>
-      <Button onClick={() => alert("1")}>Add</Button>
-    </StyleApp>
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <Input></Input>
+        <H1>فونت فارسی</H1>
+        <Button onClick={() => alert("1")}>اضافه</Button>
+        <Button onClick={() => alert("1")}>اضافه</Button>
+      </StyleApp>
+    </>
   );
 }
---
+++ apps/hotel-management/src/ui/Input.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  padding: 1rem 5rem;
+  border-radius: var(--border-radius-lg);
+  box-shadow: var(--shadow-sm);
+`;
+export default Input;
