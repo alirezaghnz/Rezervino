383abecab3c9e76089f31998f59c2daa62076df5
alirezaghnzd <changlee76301@gmail.com>
2025-08-31 11:08:03 +0330

    add error boundaries for Error handeling

---
+++ apps/customer-site/app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+//for error bounderis
+export default function Error({ error, reset }: any) {
+  return (
+    <main className="flex justify-center items-center flex-col gap-6 text-primary-800">
+      <h1 className="text-3xl font-semibold">
+        خطایی رخ داده است. لطفا بررسی کنید
+      </h1>
+      <p className="text-lg text-red-600">{error.message}</p>
+      <button
+        className="inline-block bg-accent-600 rounded-sm text-primary-800 px-6 py-3 text-lg"
+        onClick={reset}
+      >
+        دوباره تلاش کنید
+      </button>
+    </main>
+  );
+}
