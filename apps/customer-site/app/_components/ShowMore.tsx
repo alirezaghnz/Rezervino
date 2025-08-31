efc00eaa4a965c27dcf0d6209c0abc39d475ed7c
alirezaghnzd <changlee76301@gmail.com>
2025-08-31 22:37:21 +0330

    fix show more

---
+++ apps/customer-site/app/_components/ShowMore.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+const MAX_TEXT = 40;
+
+export default function ShowMore({ children }: any) {
+  const [isShowMore, setIsShowMore] = useState(false);
+
+  const display = isShowMore
+    ? children
+    : children.split(" ").slice(0, 40).join(" ") + "...";
+
+  if (children.length < MAX_TEXT) return children;
+
+  return (
+    <span>
+      {display}
+      <button
+        className="text-primary-700 border-b border-primary-400"
+        onClick={() => setIsShowMore(!isShowMore)}
+      >
+        {isShowMore ? "بستن" : "ادامه"}
+      </button>
+    </span>
+  );
+}
