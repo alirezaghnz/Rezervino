d695736f57bda18393015eeef8a47cadb403c55c
alirezaghnzd <changlee76301@gmail.com>
2025-09-21 01:41:03 +0330

    Fix allProvinces library

--- apps/customer-site/app/_components/SelectCity.tsx
+++ apps/customer-site/app/_components/SelectCity.tsx
@@ -2,14 +2,14 @@
 
 import { useEffect, useState } from "react";
 // @ts-ignore
-import iranCity from "iran-city";
+import { allProvinces } from "iran-city";
 
 export default function SelectCity({ id, name, defaultCity, className }: any) {
   const [ostans, setOstans] = useState<{ id: number; name: string }[]>([]);
 
   //load ostans on mount
   useEffect(() => {
-    setOstans(iranCity.allProvinces());
+    setOstans(allProvinces() ?? []);
   }, [setOstans]);
 
   return (
@@ -20,7 +20,7 @@ export default function SelectCity({ id, name, defaultCity, className }: any) {
       className={className}
     >
       <option value="">استان محل سکونت</option>
-      {ostans.map((p) => (
+      {ostans?.map((p) => (
         <option value={p.name} key={p.name}>
           {p.name}
         </option>
