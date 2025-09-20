9d3d516a440650a7c244a109d9698d0d40bd3a85
alirezaghnzd <changlee76301@gmail.com>
2025-09-20 20:27:17 +0330

    test auth

--- apps/customer-site/app/api/auth/[...nextauth]/route.ts
+++ apps/customer-site/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,4 @@
 export { GET, POST } from "@/app/_lib/auth";
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("AUTH_GOOGLE_ID:", process.env.AUTH_GOOGLE_ID);
