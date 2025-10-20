a4299adf6ae3ef252bb894522bf557e865d3ffe3
alirezaghnzd <changlee76301@gmail.com>
2025-10-20 15:01:35 +0330

    style: Fix some issues on DatePicker for reposnive

--- apps/customer-site/app/_components/DatePick.tsx
+++ apps/customer-site/app/_components/DatePick.tsx
@@ -8,6 +8,7 @@ import {
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useRezervation } from "../_context/RezervationContext";
+import { useEffect, useState } from "react";
 
 function isAlreadyRezerv(range: any, datesArr: Date[]) {
   return (
@@ -21,6 +22,16 @@ function isAlreadyRezerv(range: any, datesArr: Date[]) {
 
 function DatePicker({ settings, villa, rezervedDates }: any) {
   const { range, setRange, resetRange } = useRezervation();
+
+  // for repsonsive number of month: mobil = 1 , desk = 2
+  const [numberOfMonth, setNumberOfMonth] = useState<number>(1);
+  useEffect(() => {
+    const update = () => setNumberOfMonth(window.innerWidth >= 1024 ? 2 : 1);
+    update();
+    window.addEventListener("resize", update);
+    return () => window.removeEventListener("resize", update);
+  });
+
   const displayRange = isAlreadyRezerv(range, rezervedDates)
     ? undefined
     : range;
@@ -31,9 +42,9 @@ function DatePicker({ settings, villa, rezervedDates }: any) {
   const villaPriceCalculated = numNights * (regularPrice - discount);
   const { minBookingLength, maxBookingLength } = settings;
   return (
-    <div className="flex flex-col justify-between bg-primary-600 ">
+    <div className="flex flex-col justify-between backdrop-blur-md bg-primary-900/30 ">
       <DayPicker
-        className={`pt-6 place-self-center bg-white rounded-lg text-primary-900 mt-7 `}
+        className={`pt-6 place-self-center bg-white rounded-sm shadow-sm text-primary-900 mt-7 `}
         mode="range"
         animate
         navLayout="around"
@@ -48,49 +59,54 @@ function DatePicker({ settings, villa, rezervedDates }: any) {
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
-        numberOfMonths={2}
+        numberOfMonths={numberOfMonth}
         disabled={(curentDate) =>
           isPast(curentDate) ||
           rezervedDates.some((date: any) => isSameDay(date, curentDate))
         }
       />
-      <div className="flex items-center justify-between px-8 bg-accent-600 text-primary-900 h-[72px] rounded-lg">
-        <div className="flex items-baseline gap-6">
-          <p className="flex gap-2 items-baseline">
+      <div className="flex flex-col sm:flex-row items-center sm:items-center justify-between px-4 sm:px-8 bg-accent-600 text-primary-900 gap-3 py-4 mt-1">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3">
+          <div className="text-sm sm:text-base">
             {discount > 0 ? (
-              <>
-                <span className="text-1xl">تومان{regularPrice - discount}</span>
+              <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3">
+                <span className="text-base sm:text-lg">
+                  قیمت هر شب: {regularPrice - discount} تومان
+                </span>
                 <span className="line-through font-semibold text-primary-700">
-                  تومان{regularPrice}
+                  {regularPrice} تومان
                 </span>
-              </>
+              </div>
             ) : (
-              <span className="text-1xl">{regularPrice}</span>
+              <span className="text-base sm:text-lg">{regularPrice} تومان</span>
             )}
-          </p>
+          </div>
+
           {numNights ? (
-            <>
-              <p className="bg-accent-600 px-3 py-2 text-1xl">
-                <span>&times;</span> <span>{numNights}شب</span>
-              </p>
-              <p>
-                <span className="text-xs font-bold uppercase">کل قیمت:</span>{" "}
-                <span className="text-1xl font-semibold">
-                  تومان{villaPriceCalculated}
-                </span>
-              </p>
-            </>
+            <div className="flex items-center gap-3">
+              <span className="bg-accent-700 px-3 py-1 rounded text-sm">
+                {numNights} شب
+              </span>
+              <div>
+                <div className="text-xs font-bold uppercase">کل قیمت:</div>
+                <div className="text-lg font-semibold">
+                  {villaPriceCalculated} تومان
+                </div>
+              </div>
+            </div>
           ) : null}
         </div>
 
-        {range.from || range.to ? (
-          <button
-            className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
-          >
-            حذف
-          </button>
-        ) : null}
+        <div className="w-full sm:w-auto flex sm:items-center">
+          {range.from || range.to ? (
+            <button
+              className="w-full sm:w-auto border border-primary-800 py-2 px-4 text-sm font-semibold rounded"
+              onClick={() => resetRange()}
+            >
+              حذف
+            </button>
+          ) : null}
+        </div>
       </div>
     </div>
   );
--- apps/customer-site/app/_components/LoginM.tsx
+++ apps/customer-site/app/_components/LoginM.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function LoginM() {
   return (
-    <div className="grid bg-primary-800">
-      <p className="text-center text-xl self-center">
+    <div className="grid bg-primary-700 py-10">
+      <p className="text-center text-md lg:text-xl self-center">
         برای اطلاعات رزرو با گوگل اکانت خود وارد شوید
       </p>
-      <Link href="/login" className=" text-xl text-center text-accent-900">
-        <span className="px-5 rounded-sm bg-accent-400 py-1">
+      <Link href="/login" className="text-xl text-center pt-10 text-accent-900">
+        <span className="px-6  rounded-sm bg-accent-400 py-1">
           {" "}
           ورود به حساب کاربری
         </span>
