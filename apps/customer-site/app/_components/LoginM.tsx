dc968fd4e3f34f349d71873b97bef5153e90066a
alirezaghnzd <changlee76301@gmail.com>
2025-10-20 14:20:39 +0330

    style: fix some style in LoginM component

--- apps/customer-site/app/_components/LoginM.tsx
+++ apps/customer-site/app/_components/LoginM.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function LoginM() {
   return (
     <div className="grid bg-primary-800">
-      <p className="text-center text-xl py-5 self-center">
-        لطفا با گوگل اکانت خود وارد شوید
+      <p className="text-center text-xl self-center">
+        برای اطلاعات رزرو با گوگل اکانت خود وارد شوید
       </p>
-      <Link
-        href="/login"
-        className="underline text-xl text-center text-accent-900"
-      >
-        ورود
+      <Link href="/login" className=" text-xl text-center text-accent-900">
+        <span className="px-5 rounded-sm bg-accent-400 py-1">
+          {" "}
+          ورود به حساب کاربری
+        </span>
       </Link>
     </div>
   );
